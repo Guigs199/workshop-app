269f5ba08a0856bf0ee8b75ac1baed4c5701f11b
Guilherme Figueira <gui.199@hotmail.com>
2023-01-18 12:29:35 -0300

    Aula 5

--- lib/app/modules/forms/forms_module.dart
+++ lib/app/modules/forms/forms_module.dart
@@ -1,12 +1,14 @@
 import 'package:flutter_modular/flutter_modular.dart';
+import 'package:workshop_app/app/modules/forms/ui/pages/questions_page.dart';
 import 'package:workshop_app/app/modules/forms/ui/pages/user_page.dart';
+import 'package:workshop_app/app/modules/forms/ui/stores/questions_store.dart';
 import 'package:workshop_app/app/modules/forms/ui/stores/user_store.dart';
-
 import '../../shared/utils.dart';
 
 class FormsModule extends Module {
   static List<Bind> exports = [
     Bind.lazySingleton((i) => UserStore()),
+    Bind.lazySingleton((i) => QuestionsStore()),
   ];
 
   @override
@@ -14,6 +16,13 @@ class FormsModule extends Module {
 
   @override
   final List<ModularRoute> routes = [
-    ChildRoute(PodiPages.userPage(false), child: (context, args) => UserPage())
+    ChildRoute(
+      PodiPages.userPage(false), 
+      child: (context, args) => UserPage(),
+    ),
+    ChildRoute(
+      PodiPages.questionPage(false), 
+      child: (context, args) => QuestionPage(),
+    ),
   ];
 }
---
+++ lib/app/modules/forms/infra/models/choice_model.dart
@@ -0,0 +1,8 @@
+
+class ChoiceModel{
+  String char;
+  String title;
+  bool isCorrect;
+
+  ChoiceModel(this.char, this.title, this.isCorrect);
+}
